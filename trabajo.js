ab7953249ed2b746b88dff844b8ea7c8b3713a56
isaacwide <isaac.aguilard@alumno.buap.mx>
2025-08-04 15:27:53 -0600

    add fetch method

--- trabajo.js
+++ trabajo.js
@@ -1,6 +1,19 @@
 const mostrar = document.getElementById("mostrar");
 
 mostrar.addEventListener('click',e =>{
-    // aqui llamaremos al json e imprimiremos los datos
+    
 });
 
+function obtener_nombre(){
+    let nombres = [];
+    fetch('datos.json')
+    .then(response => response.json()) // convertir la respuesta a JSON
+    .then(data => {
+      // data es un array de objetos
+      nombres = data.map(datos => datos.name); // extrae solo los nombres
+      console.log(nombres); // ["Ana", "Luis", "Carlos"]
+    })
+    .catch(error => {
+      console.error('Error al cargar el JSON:', error);
+    });
+}
