f5382a13683325d87b34e71e33f617b9a5e20af3
isaacwide <isaac.aguilard@alumno.buap.mx>
2025-08-06 19:28:55 -0600

    tengo seuño we

--- trabajo.js
+++ trabajo.js
@@ -1,4 +1,31 @@
-document.getElementById("mostrar").addeventListener('submit',e=>{
+let formulario = document.getElementById("js");
 
+document.getElementById("mostrar").addEventListener('click',e=>{
+    e.preventDefault();
+     // obtenermos los valores de los input 
+     const elements = document.querySelectorAll('input[name="data"]:checked');
+     formulario.innerHTML = ""; // Limpiar el formulario antes de agregar nuevos elementos
+     elements.forEach(element => {
+        let pointer = element.value;
+        mostrar_json(pointer); 
+     });
 
 });
+
+
+function mostrar_json(pointer){
+     fetch('datos.json')
+     .then(response => response.json())
+     .then(data =>{
+        const datos=data.map(datos => datos[pointer]);
+
+        // vamos a imprimir los datos en el formulario 
+        datos.forEach(dato => {
+            let data = document.createElement("p");
+            data.textContent = dato;
+            formulario.appendChild(data);   
+        });
+
+     })
+        
+}
